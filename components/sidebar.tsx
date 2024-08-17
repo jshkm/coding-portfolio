272350eaf1bce7d0cb601a9597805f0f93a9b9b7
jshkm <2joshmail@gmail.com>
2024-08-16 22:06:34 -0400

    modal progress

---
+++ components/sidebar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { motion, AnimatePresence } from "framer-motion"
+import { useEffect, useState } from 'react'
+
+function Sidebar() {
+    const [isVisible, setIsVisible] = useState(true)
+    const [isOpen, setIsOpen] = useState(true)
+
+    const handleOpen = () => {
+        setIsOpen(true)
+    }
+
+    const handleBack = () => {
+        // sets animation to false so modal exits
+        setIsVisible(false)
+    }
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsVisible(true)
+        }
+    }, [isOpen])
+
+    return (
+        <>
+            <motion.button
+                onClick={handleOpen}
+                disabled={isOpen}>
+                <img className={'menu-icon'} src={'/icons/menu-icon.svg'} alt={'menu icon'}></img>
+            </motion.button>
+            <Dialog open={isOpen} onClose={() => console.log('no')} className="relative">
+                <AnimatePresence onExitComplete={() => setIsOpen(false)}>
+                    {isVisible && (
+                        <motion.div
+                            key='modal'
+                            initial={{ x: '-100%' }}
+                            animate={{ x: '0%' }}
+                            transition={{ type: "spring", stiffness: 77, damping: 16 }}
+                            exit={{ x: '-100%' }}
+                            className="fixed inset-0 flex items-center justify-start">
+                            <DialogPanel className="h-screen w-screen md:w-[33vw] bg-[#EAE1D5]">
+                                <div className='flex flex-col justify-between w-full h-full'>
+                                    <div className='flex flex-col justify-between h-3/4 space-y-4'>
+                                        <button onClick={handleBack}>
+                                            <img className='max-h-8' src={'/icons/menu-icon.svg'} alt={'menu icon'}></img>
+                                        </button>
+                                        <h1 className='text-6xl archivo-black font-extrabold'>cart</h1>
+                                        <p className='jost font-normal'>only ships inside the united states!</p>
+                                    </div>
+                                </div>
+                            </DialogPanel>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </Dialog>
+        </>
+    )
+}
+
+export default Sidebar
