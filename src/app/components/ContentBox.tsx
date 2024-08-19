65495774bc8be04a160c31555ef94e5c3fb8eb8e
jshkm <2joshmail@gmail.com>
2024-08-19 18:42:14 -0400

    lots of changes

--- src/app/components/ContentBox.tsx
+++ src/app/components/ContentBox.tsx
@@ -1,35 +1,61 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
-interface CardProps {
-    text: string
+interface PageType {
+    text: string;
+    icon: string;
 }
-const Card = ({ text } : CardProps) => {
+
+interface ContentBoxProps {
+    pages: PageType[]
+}
+const Card = ({ page }: { page: PageType }) => {
     return (
         <motion.button
             style={{ filter: "brightness(1)" }}
             whileHover={{ filter: "brightness(1.35)" }}
         >
-            <div className='flex justify-start h-[200px] w-[200px] bg-[#1E1F20] rounded-xl p-4'>
-                <p>{text}</p>
+            <div className='flex flex-col items-start justify-between h-[200px] w-[200px] bg-[#1E1F20] rounded-xl p-4'>
+                <p className='text-left'>{page.text}</p>
+                <div className='flex w-full justify-end'>
+                    <div className='flex items-center justify-center h-10 w-10 bg-[#131314] rounded-full'>
+                        <img className='min-h-7' src={page.icon} alt={'icon'}></img>
+                    </div>
+                </div>
             </div>
         </motion.button>
     )
 }
 
+const SearchBar = () => {
+  return (
+    <div className='bg-[#1E1F20] h-16 max-w-[824px] w-full rounded-full'>
+        
+    </div>
+  )
+}
 
-function ContentBox() {
+
+function ContentBox({ pages }: ContentBoxProps) {
     return (
-        <div className='flex flex-col justify-between items-center h-full w-full px-10 py-6 border border-red-500 '>
-            <div className='text-5xl space-y-7'>
-                <h1>Hello, welcome to Josh Kim's Portfolio!</h1>
-                <h1>How can I help you today?</h1>
+        <div className='flex flex-col justify-between items-center h-full w-full px-10 py-6'>
+            <div className='flex w-full h-10'>
+                <h1>think of something creative to put here</h1>
             </div>
-
-            <Card text='pepe'></Card>
-
-            <p>Other content</p>
-            <p>Search bar... maybe</p>
+            <div className='flex flex-col items-center w-full space-y-20'>
+                <div className='text-5xl space-y-7 text-wrap'>
+                    <h1 className='font-semibold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-red-500'>Hello, welcome to Josh Kim's Portfolio</h1>
+                    <h1 className='text-[#444746] font-semibold'>How can I help you today?</h1>
+                </div>
+                <div className='flex w-full items-center justify-center overflow-x-auto space-x-2 py-2'
+                //figure out weird scroll behavior
+                >
+                    {pages.map((page, i) => (
+                        <Card key={i} page={page}></Card>
+                    ))}
+                </div>
+            </div>
+            <SearchBar></SearchBar>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ function HomePage() {
   const [isOpen, setIsOpen] = useState(true)
   const [isVisible, setIsVisible] = useState(true)
 
+  const pages = [{ 'text': 'Who is Josh Kim?', 'icon': '/icons/person-icon.svg' },
+  { 'text': "Show me Josh's work experience?", 'icon': '/icons/code-icon.svg' },
+  { 'text': "What are Josh's projects?", 'icon': '/icons/github-icon.svg' },
+  { 'text': "Where can I find Josh's resume?", 'icon': '/icons/resume-icon.svg' }
+  ]
+
   const handleClick = () => {
     // sets animation to false so modal exits
     if (isOpen) {
@@ -36,30 +42,36 @@ function HomePage() {
         <AnimatePresence onExitComplete={() => setIsOpen(false)}>
           {isVisible && (
             <motion.div
-              className={`h-full ${isOpen ? 'w-80' : 'w-0'} bg-[#1E1F20] flex flex-col justify-center p-6 z-0`}
+              className={`h-full ${isOpen ? 'w-80' : 'w-0'} bg-[#1E1F20] flex flex-col justify-center p-6 z-0 space-y-2`}
               initial={{ x: '-100%', opacity: 0 }}
               animate={{ x: '0%', opacity: 1 }}
               transition={{ type: "spring", stiffness: 77, damping: 16 }}
               exit={{ x: '-100%', opacity: 0 }}
               layout
             >
-              <p>hello</p>
-              <p>hello</p>
-              <p>hello</p>
-              <p>hello</p>
-              <p>hello</p>
-              <p>hello</p>
-              {/* Sidebar content */}
+              <h2 className='font-medium text-[#B1B2B2] text-xl'>Recent</h2>
+              <div className='flex flex-col justify-start items-center w-full h-1/2 space-y-2'>
+                {pages.map((page, i) => (
+                  <motion.button
+                    className='flex rounded-2xl w-[300px] items-center space-x-2 py-1 pl-3 bg-[#1E1F20]'
+                    style={{ filter: "brightness(1)" }}
+                    whileHover={{ filter: "brightness(1.35)" }}
+                  >
+                    <img className='' src={'/icons/chat-icon.svg'} alt={'chat icon'}></img>
+                    <p className='text-[#B1B2B2] text-base'>{page.text}</p>
+                  </motion.button>
+                ))}
+              </div>
             </motion.div>
           )}
         </AnimatePresence>
       </div>
       <motion.div
-          className={`flex flex-grow justify-center items-center`}
-          layout
-          transition={{ type: "spring", stiffness: 77, damping: 16 }}
+        className={`flex justify-center items-center ${isVisible ? 'w-[calc(100vw-320px)]' : 'w-screen'}`}
+        layout
+        transition={{ type: "spring", stiffness: 77, damping: 16 }}
       >
-        <ContentBox></ContentBox>
+        <ContentBox pages={pages}></ContentBox>
       </motion.div>
     </div>
   )
